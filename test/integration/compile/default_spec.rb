99590364d9d01fe06a01d22c75a406fb0552ee7c
Sliim <sliim@mailoo.org>
2020-01-08 12:50:55 +0100

    test/compile: check dunstify command exists
    dunstify needs a running dunst server to work, check the command
    exists instead of checking exitcode and stdout

--- test/integration/compile/default_spec.rb
+++ test/integration/compile/default_spec.rb
@@ -3,11 +3,8 @@
 describe command 'dunst --version' do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should match(/Dunst - A customizable and lightweight notification-daemon/) }
-  its(:stderr) { should eq '' }
 end
 
-describe command 'dunstify -s' do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(/name:dunst/) }
-  its(:stderr) { should eq '' }
+describe command 'dunstify' do
+  it { should exist }
 end
