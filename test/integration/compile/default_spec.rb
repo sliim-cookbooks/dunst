ed0f7d0e81de78a8827eef63493153e965dde7e8
Sliim <sliim@mailoo.org>
2020-01-08 12:04:55 +0100

    test: ensure stderr is empty for commands in compile suite

--- test/integration/compile/default_spec.rb
+++ test/integration/compile/default_spec.rb
@@ -3,9 +3,11 @@
 describe command 'dunst --version' do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should match(/Dunst - A customizable and lightweight notification-daemon/) }
+  its(:stderr) { should eq '' }
 end
 
 describe command 'dunstify -s' do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should match(/name:dunst/) }
+  its(:stderr) { should eq '' }
 end
