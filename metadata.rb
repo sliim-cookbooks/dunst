5c004d8be747b4dedd41eb525732dae13c623e96
Sliim <sliim@mailoo.org>
2020-01-08 11:23:24 +0100

    Fix cookstyle

--- metadata.rb
+++ metadata.rb
@@ -3,18 +3,10 @@ maintainer 'Sliim'
 maintainer_email 'sliim@mailoo.org'
 license 'Apache-2.0'
 description 'Installs/Configures dunst'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-chef_version '>= 12.5' if respond_to?(:chef_version)
+chef_version '>= 12.5'
 version '0.2.1'
-
-recipe 'dunst::default', 'Do nothing'
-recipe 'dunst::package', 'Installs Dunst from package'
-recipe 'dunst::compile', 'Installs Dunst from sources'
-recipe 'dunst::config', 'Configures Dunst for a specific user'
 
 supports 'debian'
 
-source_url 'https://github.com/sliim-cookbooks/dunst' if
-  respond_to?(:source_url)
-issues_url 'https://github.com/sliim-cookbooks/dunst/issues' if
-    respond_to?(:issues_url)
+source_url 'https://github.com/sliim-cookbooks/dunst'
+issues_url 'https://github.com/sliim-cookbooks/dunst/issues'
--- recipes/compile.rb
+++ recipes/compile.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Cookbook Name:: dunst
+# Cookbook:: dunst
 # Recipe:: compile
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
